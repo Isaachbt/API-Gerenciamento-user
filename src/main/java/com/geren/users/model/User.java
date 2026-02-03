2082b1edc7f1d7de9c460334f4c7485a4918521e
Isaac <isaac.silva1478@gmail.com>
2026-02-02 21:27:44 -0300

    add dados class user

--- src/main/java/com/geren/users/model/User.java
+++ src/main/java/com/geren/users/model/User.java
@@ -1,4 +1,23 @@
 package com.geren.users.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
