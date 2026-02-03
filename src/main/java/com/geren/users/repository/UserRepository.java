28d105c30dc1ba722f5670d256cff6230d6318c6
Isaac <isaac.silva1478@gmail.com>
2026-02-02 21:29:58 -0300

    add dados class UserRepository

--- src/main/java/com/geren/users/repository/UserRepository.java
+++ src/main/java/com/geren/users/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.geren.users.repository;
 
-public interface UserRepository {
+import com.geren.users.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
