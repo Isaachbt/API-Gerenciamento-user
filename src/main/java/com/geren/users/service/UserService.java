a1c12aaf94146bcebbab959205df2c34db524d6f
Isaac <isaac.silva1478@gmail.com>
2026-02-02 21:28:02 -0300

    add dados class userService

--- src/main/java/com/geren/users/service/UserService.java
+++ src/main/java/com/geren/users/service/UserService.java
@@ -1,4 +1,16 @@
 package com.geren.users.service;
 
+import com.geren.users.model.User;
+import com.geren.users.model.dto.UserDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
+
+    User createUser(UserDTO user);
+    List<User> getAllUsers();
+    User getUserById(UUID id);
+    User updateUser(UUID id, UserDTO userDTO);
+    void deleteUser(UUID id);
 }
--- src/main/java/com/geren/users/service/imp/UserServiceImp.java
+++ src/main/java/com/geren/users/service/imp/UserServiceImp.java
@@ -1,4 +1,42 @@
 package com.geren.users.service.imp;
 
-public class UserServiceImp {
+import com.geren.users.model.User;
+import com.geren.users.model.dto.UserDTO;
+import com.geren.users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImp implements UserService {
+    @Autowired
+    private UserService userService;
+
+
+    @Override
+    public User createUser(UserDTO user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public User getUserById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(UUID id, UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(UUID id) {
+
+    }
 }
