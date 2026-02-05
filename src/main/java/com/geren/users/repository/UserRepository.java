eb0867bc635d460bb25cbd43b5b5749364099816
Isaac <isaac.silva1478@gmail.com>
2026-02-04 21:27:56 -0300

    existByEmail and findByEmail criado em repository

--- src/main/java/com/geren/users/repository/UserRepository.java
+++ src/main/java/com/geren/users/repository/UserRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+     boolean existsByEmail(String email);
 }
