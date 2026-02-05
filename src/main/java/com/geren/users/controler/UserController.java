6aae2a07bbe4c6c75267dbdbf28f4289c9d5bd1e
Isaac <isaac.silva1478@gmail.com>
2026-02-04 21:27:04 -0300

    metodos UserController criados

--- src/main/java/com/geren/users/controler/UserController.java
+++ src/main/java/com/geren/users/controler/UserController.java
@@ -1,5 +1,65 @@
 package com.geren.users.controler;
 
+import com.geren.users.model.User;
+import com.geren.users.model.dto.UserDTO;
+import com.geren.users.model.dto.UserResponseDTO;
+import com.geren.users.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("user/")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("creatUser")
+    public ResponseEntity<Object> createUser(@RequestBody @Valid UserDTO user){
+
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("getAllUser")
+    public ResponseEntity<List<User>> getAllUser(){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("getOneUser/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") UUID id){
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable UUID id){
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
 }
