9b7d804677c8512921f3804ff5920b65f78de638
Isaac <isaac.silva1478@gmail.com>
2026-02-22 13:55:30 -0300

    feat: metodos login,createUser e generateResetToken

--- src/main/java/com/geren/users/service/UserService.java
+++ src/main/java/com/geren/users/service/UserService.java
@@ -1,15 +1,17 @@
 package com.geren.users.service;
 
+import com.geren.users.dto.LoginDTO;
 import com.geren.users.model.User;
-import com.geren.users.model.dto.UserDTO;
-import com.geren.users.model.dto.UserResponseDTO;
+import com.geren.users.dto.UserDTO;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
 
-    User createUser(UserDTO user);
+    void createUser(UserDTO userDTO);
+    String login(LoginDTO loginDTO);
+    String generateResetToken(UUID id);
     List<User> getAllUsers();
     User getUserById(UUID id);
     User updateUser(UUID id, UserDTO userDTO);
--- src/main/java/com/geren/users/service/imp/UserServiceImp.java
+++ src/main/java/com/geren/users/service/imp/UserServiceImp.java
@@ -1,13 +1,23 @@
 package com.geren.users.service.imp;
 
+import com.geren.users.dto.LoginDTO;
+import com.geren.users.enums.RoleEnum;
 import com.geren.users.exception.EmailAlreadyExistsException;
+import com.geren.users.exception.ErroCadastro;
 import com.geren.users.exception.NotFound;
+import com.geren.users.exception.PasswordIncorreta;
 import com.geren.users.model.User;
-import com.geren.users.model.dto.UserDTO;
+import com.geren.users.dto.UserDTO;
 import com.geren.users.repository.UserRepository;
+import com.geren.users.service.AuthenticationService;
 import com.geren.users.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -18,19 +28,62 @@ public class UserServiceImp implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @Override
-    public User createUser(UserDTO user) {
-
-
-        if (userRepository.existsByEmail(user.email())) {
+    public void createUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.email())) {
             throw new EmailAlreadyExistsException("E-mail existente");
         }
 
-        User usr = new User();
-        BeanUtils.copyProperties(user, usr);
-        return userRepository.save(usr);
+        var passWordHas = passwordEncoder.encode(userDTO.password());
+        var authUser = new User();
+        BeanUtils.copyProperties(userDTO, authUser);
+        authUser.setPassword(passWordHas);
+        authUser.setRole(RoleEnum.USER);
+
+        try{
+            userRepository.save(authUser);
+        }catch (Exception e){
+            throw new ErroCadastro("Não foi possivl cadastrar usuario.");
+        }
     }
+
+    @Override
+    public String login(LoginDTO loginDTO) {
+        try {
+
+            var authenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            loginDTO.email(),
+                            loginDTO.password()
+                    );
+
+            authenticationManager.authenticate(authenticationToken);
+
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Email ou senha inválidos");
+        }
+
+        User user = userRepository.findByEmail(loginDTO.email())
+                .orElseThrow(() -> new UsernameNotFoundException("Email ou senha inválidos"));
+
+        user.setOnline(true);
+        userRepository.save(user);
+
+        return authenticationService.login(loginDTO);
+    }
+
+    @Override
+    public String generateResetToken(UUID id) {
+        return "";
+    }
+
 
     @Override
     public List<User> getAllUsers() {
