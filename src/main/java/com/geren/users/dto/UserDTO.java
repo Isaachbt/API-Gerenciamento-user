32545e731f695b9cf5a9b3c759a381611d7834b6
Isaac <isaac.silva1478@gmail.com>
2026-02-22 14:00:00 -0300

    feat: novos campos no UserDTO cpf e dataNascimento

--- src/main/java/com/geren/users/dto/UserDTO.java
+++ src/main/java/com/geren/users/dto/UserDTO.java
@@ -1,8 +1,10 @@
-package com.geren.users.model.dto;
+package com.geren.users.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
 public record UserDTO(@NotBlank(message = "Nome necessario.") String name,
+                      @NotBlank(message = "CPF necessario.") String cpf,
+                      @NotBlank(message = "Data de nascimento necessario.") String dataNascimento,
                       @NotBlank(message = "E-mail necessario.") String email,
                       @NotBlank(message = "Senhha necessario.") String password) {
 }
