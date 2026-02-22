cadc0ae42d411bd19cbd3daf5a58dfaeb3726eef
Isaac <isaac.silva1478@gmail.com>
2026-02-22 13:57:48 -0300

    feat: new metodo findByEmail

--- src/main/java/com/geren/users/repository/UserRepository.java
+++ src/main/java/com/geren/users/repository/UserRepository.java
@@ -3,13 +3,11 @@ package com.geren.users.repository;
 import com.geren.users.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
+     Optional<User> findByEmail(String email);
      boolean existsByEmail(String email);
 }
