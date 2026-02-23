93c197f0afd55476698f837d073fe1b81662fcf5
Isaac <isaac.silva1478@gmail.com>
2026-02-22 21:50:42 -0300

    novo metodo add, deleteUser AuthenticationController.java

--- src/main/java/com/geren/users/controler/AuthenticationController.java
+++ src/main/java/com/geren/users/controler/AuthenticationController.java
@@ -1,11 +1,11 @@
 package com.geren.users.controler;
 
+import com.geren.users.dto.ForgotPasswordDTO;
 import com.geren.users.dto.LoginDTO;
+import com.geren.users.dto.ResetPasswordDTO;
 import com.geren.users.dto.UserDTO;
 import com.geren.users.service.UserService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,28 +15,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("auth")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     @Autowired
     private UserService userService;
 
 
-    @PostMapping("creatUser")
+    @PostMapping("/creatUser")
     public ResponseEntity<Object> createUser(@RequestBody @Valid UserDTO user){
          userService.createUser(user);
          return ResponseEntity.status(HttpStatus.CREATED).body("Usuario criado com sucesso: ");
     }
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid LoginDTO user){
         return ResponseEntity.status(HttpStatus.OK).body(userService.login(user));
     }
 
-    @PostMapping("forgot-password/{id}")
-    public ResponseEntity<Object> forgotPassword(@PathVariable(value = "id")@NotNull UUID id){
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@RequestBody ForgotPasswordDTO dto) {
 
-        return ResponseEntity.status(HttpStatus.OK).body("Senha alterada com sucesso");
+        userService.generateResetToken(dto.email());
+
+        return ResponseEntity.ok("Se o e-mail existir, você receberá instruções para redefinir a senha."
+        );
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Object> resetPassword(@RequestBody ResetPasswordDTO dto){
+        userService.resetPassword(dto.token(), dto.newPassword());
+        return ResponseEntity.ok("Senha redefinida com sucesso.");
+    }
+
+    @DeleteMapping("/delete-user")
+    public ResponseEntity<Object> deleteUser(){
+        userService.deleteUser();
+        return ResponseEntity.status(HttpStatus.OK).body("Usuario deletado com sucesso.");
     }
 
 }
